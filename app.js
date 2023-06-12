226e261385e9157b09721138dcdb6866607f2a22
Eugene-Guryanov <103857729+Eugene-Guryanov@users.noreply.github.com>
2023-06-12 14:18:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use((req, res, next) => {
   }
   next();
 });
-const { PORT = 3000, mongoAddress = 'mongodb://0.0.0.0:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, mongoAddress = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
