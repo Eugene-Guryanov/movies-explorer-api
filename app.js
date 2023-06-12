ee7b271ae068b1baf49e917c5d51e3668f5b7b28
Eugene-Guryanov <103857729+Eugene-Guryanov@users.noreply.github.com>
2023-06-12 11:20:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const { centralizedErrorHandler } = require('./middlewares/centralizedErrorHandl
 const app = express();
 const allowedCors = [
   'http://localhost:3001',
+  'https://localhost:3001',
+  'http://localhost:3000',
+  'https://localhost:3000',
   'https://ges.nomoredomains.rocks',
   'http://ges.nomoredomains.rocks',
 ];
